5b3f800c7801294871e888037983e8bc133d624c
Hichem2h <hichem2h@gmail.com>
2018-03-31 00:26:22 +0100

    configure forums

---
+++ config.php
@@ -0,0 +1,22 @@
+<?php return array (
+  'debug' => false,
+  'database' => 
+  array (
+    'driver' => 'mysql',
+    'host' => 'us-cdbr-iron-east-05.cleardb.net',
+    'database' => 'heroku_7fc8eb2ecbff2aa',
+    'username' => 'b7331d89c77b36',
+    'password' => '82da7aa8',
+    'charset' => 'utf8mb4',
+    'collation' => 'utf8mb4_unicode_ci',
+    'prefix' => '',
+    'port' => '3306',
+    'strict' => false,
+  ),
+  'url' => 'http://localhost:8000',
+  'paths' => 
+  array (
+    'api' => 'api',
+    'admin' => 'admin',
+  ),
+);
